443f59e324927934962147e763ab2487d6053ee3
Beru Hinode <windowz414@1337.lgbt>
2022-09-20 08:43:07 +0000

    rosemary-firmware/makefile: Define only if target is `rosemary`
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
-define target_radio_files
-$(notdir \
-        $(wildcard $(LOCAL_PATH)/*.bin) \
-        $(wildcard $(LOCAL_PATH)/*.img)
-)
-endef
+ifneq ($(shell echo $(TARGET_PRODUCT) | grep rosemary),)
+    define target_radio_files
+    $(notdir \
+            $(wildcard $(LOCAL_PATH)/*.bin) \
+            $(wildcard $(LOCAL_PATH)/*.img)
+    )
+    endef
 
--include $(LOCAL_PATH)/rosemary-firmware.mk
+    -include $(LOCAL_PATH)/rosemary-firmware.mk
+endif
