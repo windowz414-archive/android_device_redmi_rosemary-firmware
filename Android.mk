b983fd47b3f5c4d227407c4dc229c88513db4eda
Beru Hinode <windowz414@1337.lgbt>
2022-09-21 20:21:07 +0000

    Revert "rosemary-firmware/makefile: Define only if target is `rosemary`"
    This reverts commit 443f59e324927934962147e763ab2487d6053ee3.

--- Android.mk
+++ Android.mk
@@ -1,12 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(shell echo $(TARGET_PRODUCT) | grep rosemary),)
-    define target_radio_files
-    $(notdir \
-            $(wildcard $(LOCAL_PATH)/*.bin) \
-            $(wildcard $(LOCAL_PATH)/*.img)
-    )
-    endef
+define target_radio_files
+$(notdir \
+        $(wildcard $(LOCAL_PATH)/*.bin) \
+        $(wildcard $(LOCAL_PATH)/*.img)
+)
+endef
 
-    -include $(LOCAL_PATH)/rosemary-firmware.mk
-endif
+-include $(LOCAL_PATH)/rosemary-firmware.mk
